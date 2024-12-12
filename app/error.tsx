d0354727a24cc4c864a29e7872e574bb5ecb7fac
Vladyslav Balabanov <vladyslav.balabanov@nure.ua>
2024-12-12 17:06:13 +0200

    Add profile view & logout

--- app/error.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { ErrorView } from '@/views/common/ErrorView';
-
-export default function Error({ error }: { error: Error & { digest?: string }; reset: () => void }) {
-  return <ErrorView error={error} />;
-}

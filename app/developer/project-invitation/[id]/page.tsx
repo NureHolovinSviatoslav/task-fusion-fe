1d1c9800f7f50ae9302e041c6a88b1dc358ea379
Vladyslav Balabanov <vladyslav.balabanov@nure.ua>
2024-12-20 13:02:04 +0200

    Integrate with payments & Stripe API

--- app/developer/project-invitation/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { DeveloperProjectInvitationView } from '@/views/invites/DeveloperProjectInvitationView';
-
-export default function Page(props: { params: { id: string } }) {
-  return <DeveloperProjectInvitationView inviteId={props.params.id} />;
-}
